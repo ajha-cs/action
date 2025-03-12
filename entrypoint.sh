91bceff79c7fff1c7311c51fd371bafe3fa84637
ajha-cs <ajha@cloudsmith.io>
2025-03-12 14:33:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -120,6 +120,7 @@ function check_required_options {
 function execute_push {
   echo "hello"
   echo $format
+  echo "hello2"
   check_required_options format owner repo file
   echo "hello1"
   local context="${options["owner"]}/${options["repo"]}"
